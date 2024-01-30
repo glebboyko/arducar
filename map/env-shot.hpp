39d366bdd2e2db0dc4bbcb4acae8523f8b59338e
Gleb Boiko <glebboyko@icloud.com>
2024-01-30 14:10:52 +0300

    Fixed EnvShot class

--- map/env-shot.hpp
+++ map/env-shot.hpp
@@ -6,18 +6,19 @@
 
 class EnvShot {
  public:
-  EnvShot(size_t size_x, size_t size_y, float px_per_mm);
+  EnvShot(int32_t size_x, int32_t size_y, float px_per_mm);
 
-  void AddMeasure(int deg, int mm_dist,
+  void AddMeasure(int deg, int32_t mm_dist,
                   const Primitives::Coord& mm_radar_coord);
 
   void CreateImage(const char* image_name) const;
 
  private:
   struct PixelData {
-    bool is_empty;
-    int dist;
+    bool is_border = false;
+    int32_t dist = INT32_MAX;
   };
 
   std::vector<std::vector<PixelData>> shot_;
+  float px_per_mm_;
 };
