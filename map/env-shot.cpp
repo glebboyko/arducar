365ef8c125a8c083596bd92af139ea8b1da3690d
Gleb Boiko <glebboyko@icloud.com>
2024-01-30 16:01:56 +0300

    Implemented EnvShot methods, fixed args

--- map/env-shot.cpp
+++ map/env-shot.cpp
@@ -1,8 +1,17 @@
 #include "env-shot.hpp"
 
-#include "image/image-creator.hpp"
+#include <tuple>
 
-namespace EnvShot {
+#include "image-creator.hpp"
+#include "primitives.hpp"
+
+EnvShot::EnvShot(int size_x, int size_y, float px_per_mm)
+    : px_per_mm_(px_per_mm) {
+  map_.resize(size_x);
+  for (int32_t i = 0; i < size_y; ++i) {
+    map_[i].resize(size_y);
+  }
+}
 
 const double kPi = 3.1415926535;
 const int kDegInCircle = 360;
@@ -12,56 +21,65 @@ double DegToRad(double deg) {
   return rad;
 }
 
-std::vector<Primitives::Segment> EnvShotProcessing(
-    const std::vector<LaserRangeInf>& raw_env_shot,
-    const Primitives::Coord& kCenter) {
-  std::vector<Primitives::Segment> result;
-  result.reserve(raw_env_shot.size());
+void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
+                         const Primitives::Coord& mm_radar_coord) {
+  double left_deg = deg - (deg_width / 2);
+  double right_deg = deg + (deg_width / 2);
 
-  for (int i = 0; i < raw_env_shot.size(); ++i) {
-    int curr_deg = raw_env_shot[i].deg;
-    int prev_deg = (i > 0 ? raw_env_shot[i - 1] : raw_env_shot.back()).deg;
-    if (prev_deg > curr_deg) {
-      prev_deg += kDegInCircle;
+  double dist = static_cast<double>(mm_dist) * px_per_mm_;
+
+  Primitives::Coord radar_coord = mm_radar_coord * px_per_mm_;
+
+  Primitives::Coord left_coord = {
+      static_cast<int>(mm_radar_coord.x * px_per_mm_ +
+                       (cos(DegToRad(left_deg)) * dist)),
+      static_cast<int>(mm_radar_coord.y * px_per_mm_ +
+                       (sin(DegToRad(left_deg)) * dist))};
+
+  Primitives::Coord right_coord = {
+      static_cast<int>(mm_radar_coord.x * px_per_mm_ +
+                       (cos(DegToRad(right_deg)) * dist)),
+      static_cast<int>(mm_radar_coord.y * px_per_mm_ +
+                       (sin(DegToRad(right_deg)) * dist))};
+
+  Primitives::Segment border(left_coord, right_coord);
+  for (const auto& border_coord : border.GetGraphic()) {
+    Primitives::Segment generatrix(radar_coord, border_coord);
+    bool valid = true;
+
+    for (const auto& [x, y] : generatrix.GetGraphic()) {
+      if (x < 0 || y < 0 || x >= map_.size() || y >= map_[0].size()) {
+        continue;
+      }
+
+      PixelData& pixel = map_[x][y];
+      int px_dist = Primitives::GetDistance(radar_coord, {x, y});
+
+      if (pixel.is_border && pixel.dist <= px_dist) {
+        valid = false;
+        break;
+      } else if (pixel.dist > px_dist) {
+        pixel.dist = px_dist;
+        pixel.is_border = false;
+      }
     }
 
-    int next_deg = (i + 1 != raw_env_shot.size() ? raw_env_shot[i + 1]
-                                                 : raw_env_shot.front())
-                       .deg;
-    if (next_deg < curr_deg) {
-      next_deg += kDegInCircle;
+    if (!valid) {
+      continue;
     }
 
-    int left_deg = (prev_deg + curr_deg) / 2;
-    int right_deg = (curr_deg + next_deg) / 2;
-    int dist = raw_env_shot[i].dist;
-
-    Primitives::Coord left_coord = {
-        static_cast<int>(kCenter.x + (cos(DegToRad(left_deg)) * dist)),
-        static_cast<int>(kCenter.y + (sin(DegToRad(left_deg)) * dist))};
-
-    Primitives::Coord right_coord = {
-        static_cast<int>(kCenter.x + (cos(DegToRad(right_deg)) * dist)),
-        static_cast<int>(kCenter.y + (sin(DegToRad(right_deg)) * dist))};
-
-    result.push_back(Primitives::Segment(left_coord, right_coord));
+    double px_dist = Primitives::GetDistance(radar_coord, border_coord);
+    PixelData& pixel = map_[border_coord.x][border_coord.y];
+    if (pixel.dist > px_dist) {
+      pixel.dist = px_dist;
+      pixel.is_border = true;
+    }
   }
-  return result;
 }
 
-void DisplayImage(const std::vector<Primitives::Segment>& shot,
-                  const Primitives::Coord& k_center,
-                  const char* file_name) {
-  ImageCreator image_creator(1, 1, k_center * 2);
-
-  for (int i = 0; i < shot.size(); ++i) {
-    image_creator.Draw(shot[i]);
-    Primitives::Segment connector(shot[i].GetB(),
-                                  shot[(i + 1) % shot.size()].GetA());
-    image_creator.Draw(connector);
-  }
-
-  image_creator.CreateImage(file_name);
+void EnvShot::CreateImage(const char* image_name) const {
+  ::CreateImage(
+      image_name, map_, map_.size(), map_[0].size(), [](const PixelData& data) {
+        return data.is_border ? std::tuple(0, 0, 0) : std::tuple(255, 255, 255);
+      });
 }
-
-}  // namespace EnvShot
--- map/env-shot.hpp
+++ map/env-shot.hpp
@@ -6,9 +6,9 @@
 
 class EnvShot {
  public:
-  EnvShot(int32_t size_x, int32_t size_y, float px_per_mm);
+  EnvShot(int size_x, int size_y, float px_per_mm);
 
-  void AddMeasure(int deg, int32_t mm_dist,
+  void AddMeasure(double deg, int mm_dist, double deg_width,
                   const Primitives::Coord& mm_radar_coord);
 
   void CreateImage(const char* image_name) const;
@@ -16,9 +16,9 @@ class EnvShot {
  private:
   struct PixelData {
     bool is_border = false;
-    int32_t dist = INT32_MAX;
+    int dist = INT_MAX;
   };
 
-  std::vector<std::vector<PixelData>> shot_;
+  std::vector<std::vector<PixelData>> map_;
   float px_per_mm_;
 };
