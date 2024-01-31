296489ff543fb12cf203893c67fa54e82e9100c0
Gleb Boiko <glebboyko@icloud.com>
2024-01-31 19:11:40 +0300

    Fixed EnvShot AddMeasure method

--- map/env-shot.cpp
+++ map/env-shot.cpp
@@ -21,6 +21,52 @@ double DegToRad(double deg) {
   return rad;
 }
 
+void FulfillLine(std::vector<Primitives::Coord>& figure,
+                 const Primitives::Coord& right_coord) {}
+
+std::vector<Primitives::Coord> GetTriangle(Primitives::Coord a,
+                                           Primitives::Coord b,
+                                           Primitives::Coord c) {
+  auto ab_graph = Primitives::Segment(a, b).GetGraphic();
+  auto ac_graph = Primitives::Segment(a, c).GetGraphic();
+  auto bc_graph = Primitives::Segment(b, c).GetGraphic();
+
+  std::vector<Primitives::Coord> border;
+  border.reserve(ab_graph.size() + ac_graph.size() + bc_graph.size());
+  border.insert(border.cend(), ab_graph.begin(), ab_graph.end());
+  border.insert(border.cend(), ac_graph.begin(), ac_graph.end());
+  border.insert(border.cend(), bc_graph.begin(), bc_graph.end());
+
+  std::sort(
+      border.begin(), border.end(),
+      [](const Primitives::Coord& first, const Primitives::Coord& second) {
+        return first.y != second.y ? first.y < second.y : first.x < second.x;
+      });
+
+  std::vector<Primitives::Coord> triangle;
+  triangle.reserve(border.size());
+  for (const auto& coord : border) {
+    if (triangle.empty() || triangle.back().y != coord.y) {
+      triangle.push_back(coord);
+      continue;
+    }
+    while (triangle.back().x < coord.x) {
+      triangle.push_back({triangle.back().x + 1, triangle.back().y});
+    }
+  }
+  std::sort(
+      triangle.begin(), triangle.end(),
+      [a](const Primitives::Coord& first, const Primitives::Coord& second) {
+        auto f_dist = Primitives::GetDistance(a, first);
+        auto s_dist = Primitives::GetDistance(a, second);
+        if (f_dist != s_dist) {
+          return f_dist < s_dist;
+        }
+        return first.y != second.y ? first.y < second.y : first.x < second.x;
+      });
+  return triangle;
+}
+
 void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
                          const Primitives::Coord& mm_radar_coord) {
   double left_deg = deg - (deg_width / 2);
@@ -45,12 +91,16 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
   static size_t call_num = 0;
   call_num = (call_num + 1) % UINT64_MAX;
 
-  Primitives::Segment border(left_coord, right_coord);
-  for (const auto& border_coord : border.GetGraphic()) {
-    Primitives::Segment generatrix(radar_coord, border_coord);
+  auto border_graphic =
+      Primitives::Segment(left_coord, right_coord).GetGraphic();
+  for (auto iter = border_graphic.begin(); iter != --border_graphic.end();
+       ++iter) {
+    auto tmp_iter = iter;
+    Primitives::Coord border[2] = {*tmp_iter, *(++tmp_iter)};
+
     bool valid = true;
 
-    for (const auto& [x, y] : generatrix.GetGraphic()) {
+    for (const auto& [x, y] : GetTriangle(radar_coord, border[0], border[1])) {
       if (x < 0 || y < 0 || x >= map_.size() || y >= map_[0].size()) {
         continue;
       }
@@ -77,17 +127,19 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
     if (!valid) {
       continue;
     }
-    if (border_coord.x < 0 || border_coord.y < 0 ||
-        border_coord.x >= map_.size() || border_coord.y >= map_[0].size()) {
-      continue;
-    }
+    for (int i = 0; i < 2; ++i) {
+      if (border[i].x < 0 || border[i].y < 0 || border[i].x >= map_.size() ||
+          border[i].y >= map_[0].size()) {
+        continue;
+      }
 
-    double px_dist = Primitives::GetDistance(radar_coord, border_coord);
-    PixelData& pixel = map_[border_coord.x][border_coord.y];
-    if (pixel.dist >= px_dist || pixel.call_identifier == call_num) {
-      pixel.dist = px_dist;
-      pixel.is_border = true;
-      pixel.call_identifier = call_num;
+      double px_dist = Primitives::GetDistance(radar_coord, border[i]);
+      PixelData& pixel = map_[border[i].x][border[i].y];
+      if (pixel.dist >= px_dist || pixel.call_identifier == call_num) {
+        pixel.dist = px_dist;
+        pixel.is_border = true;
+        pixel.call_identifier = call_num;
+      }
     }
   }
 }
