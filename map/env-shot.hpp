06ccda8e021e4f64b72f812b1d9e107096c55abe
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-16 02:45:43 +0300

    Fixed namespaces

--- map/env-shot.hpp
+++ map/env-shot.hpp
@@ -10,12 +10,12 @@ class EnvShot {
   EnvShot(int size_x, int size_y, float px_per_mm, int border_thickness = 1);
 
   void AddMeasure(double deg, int mm_dist, double deg_width,
-                  const Primitives::Coord& mm_radar_coord,
+                  const PTIT::Coord& mm_radar_coord,
                   unsigned char* bitmap = nullptr);
 
   void CreateImage(const char* image_name) const;
 
-  static RGB GetRandomColor() noexcept;
+  static PTIT::RGB GetRandomColor() noexcept;
 
  private:
   struct PixelData {
@@ -26,11 +26,11 @@ class EnvShot {
 
   std::vector<std::vector<PixelData>> map_;
 
-  std::vector<RGB> sectors_color_;
+  std::vector<PTIT::RGB> sectors_color_;
   float px_per_mm_;
   int border_thickness_;
 
-  void SetPixel(unsigned char* bitmap, int x, int y, RGB color) const noexcept;
+  void SetPixel(unsigned char* bitmap, int x, int y, PTIT::RGB color) const noexcept;
 
   bool IsPixelInMap(int x, int y) const noexcept;
 };
