b182e9b96282b9f127a733062a90ba235ca83120
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-16 22:27:20 +0300

    Implemented types for communication

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Arducar
+
+## Visualization
+
+### Communication algorithm
+
+1. Client: Sends client type
+2. Car: Sends map size
+3. Query loop: Car: Sends `(QUERY_TYPE, DATA)`
--- connection_support/support.hpp
+++ connection_support/support.hpp
@@ -1,8 +1,44 @@
 #pragma once
 
+#include <string>
+
+#include "primitives.hpp"
+
 namespace SPRT {
 
+const std::string kServerAddress = "192.168.1.100";
 const int kTcpPort = 3470;
+
 enum TcpClientTypes { Visualizer, Arduino };
+
+enum QueryType {
+  QRadarScan,  // {Позиция радара, Угол поворота, Ширина снимка, Расстояние}
+  QWorkingArea,  // std::list<Coord> границ
+  QCarPosition,  // {Позиция машинки, угол поворота машинки}
+  QDestination,  // Позиция точки назначения
+  QRoute,        // std::list<Coord> точки маршрута
+  QStatus        // std::string статус
+};
+
+struct RadarScan {
+  PTIT::Coord position;
+  double deg;
+  double radar_width;
+
+  int distance;
+};
+
+using WorkingArea = std::list<PTIT::Coord>;
+
+struct CarPosition {
+  PTIT::Coord position;
+  double deg;
+};
+
+using Destination = PTIT::Coord;
+
+using Route = std::list<PTIT::Coord>;
+
+using Status = std::string;
 
 }  // namespace SPRT
