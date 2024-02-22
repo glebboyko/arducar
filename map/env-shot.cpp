9338fcd0f6fb23210ad040351dbf3007a6b5fefd
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-23 00:42:14 +0300

    Fixed EnvShot

--- map/env-shot.cpp
+++ map/env-shot.cpp
@@ -1,75 +1,37 @@
 #include "env-shot.hpp"
 
-#include <iostream>
-#include <random>
+#include <algorithm>
+#include <map>
 #include <tuple>
 
-#include "image-creator.hpp"
-#include "primitives.hpp"
+#include "ptit_lib/source/supply.hpp"
 
-EnvShot::EnvShot(int size_x, int size_y, float px_per_mm, int border_thickness,
-                 unsigned char* bitmap_scan_color,
-                 unsigned char* bitmap_scan_alpha,
-                 unsigned char* bitmap_border_alpha)
-    : px_per_mm_(px_per_mm),
-      border_thickness_(border_thickness),
-      bitmap_scan_color_(bitmap_scan_color),
-      bitmap_scan_alpha_(bitmap_scan_alpha),
-      bitmap_border_alpha_(bitmap_border_alpha) {
-  map_.resize(size_x);
-  for (int32_t i = 0; i < size_y; ++i) {
-    map_[i].resize(size_y);
-  }
-}
+EnvShot::EnvShot(BM::Bitmap& bitmap, float px_per_mm, int border_thickness)
+    : bitmap_(bitmap),
+      px_per_mm_(px_per_mm),
+      border_thickness_(border_thickness) {}
 
-const double kPi = 3.1415926535;
-const int kDegInCircle = 360;
+std::vector<PTIT::Coord> GetSortedTriangle(const PTIT::Coord& base,
+                                           const PTIT::Coord& b,
+                                           const PTIT::Coord& c) {
+  auto list_sector = PTIT::FulfillArea(PTIT::Triangle(base, b, c).GetGraphic());
 
-double DegToRad(double deg) {
-  double rad = deg * kPi / (kDegInCircle / 2);
-  return rad;
-}
+  std::vector<PTIT::Coord> vec_sector;
+  vec_sector.reserve(list_sector.size());
+  vec_sector.assign(list_sector.begin(), list_sector.end());
 
-std::vector<PTIT::Coord> GetTriangle(PTIT::Coord a, PTIT::Coord b,
-                                     PTIT::Coord c) {
-  auto ab_graph = PTIT::Segment(a, b).GetGraphic();
-  auto ac_graph = PTIT::Segment(a, c).GetGraphic();
-  auto bc_graph = PTIT::Segment(b, c).GetGraphic();
-
-  std::vector<PTIT::Coord> border;
-  border.reserve(ab_graph.size() + ac_graph.size() + bc_graph.size());
-  border.insert(border.cend(), ab_graph.begin(), ab_graph.end());
-  border.insert(border.cend(), ac_graph.begin(), ac_graph.end());
-  border.insert(border.cend(), bc_graph.begin(), bc_graph.end());
-
-  std::sort(border.begin(), border.end(),
-            [](const PTIT::Coord& first, const PTIT::Coord& second) {
-              return first.y != second.y ? first.y < second.y
-                                         : first.x < second.x;
-            });
-
-  std::vector<PTIT::Coord> triangle;
-  triangle.reserve(border.size());
-  for (const auto& coord : border) {
-    if (triangle.empty() || triangle.back().y != coord.y) {
-      triangle.push_back(coord);
-      continue;
-    }
-    while (triangle.back().x < coord.x) {
-      triangle.push_back({triangle.back().x + 1, triangle.back().y});
-    }
-  }
-  std::sort(triangle.begin(), triangle.end(),
-            [a](const PTIT::Coord& first, const PTIT::Coord& second) {
-              auto f_dist = PTIT::GetDistance(a, first);
-              auto s_dist = PTIT::GetDistance(a, second);
+  std::sort(vec_sector.begin(), vec_sector.end(),
+            [base](const PTIT::Coord& first, const PTIT::Coord& second) {
+              auto f_dist = PTIT::GetDistance(base, first);
+              auto s_dist = PTIT::GetDistance(base, second);
               if (f_dist != s_dist) {
                 return f_dist < s_dist;
               }
               return first.y != second.y ? first.y < second.y
                                          : first.x < second.x;
             });
-  return triangle;
+
+  return vec_sector;
 }
 
 void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
@@ -82,19 +44,17 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
 
   PTIT::Coord radar_coord = mm_radar_coord * px_per_mm_;
 
-  PTIT::Coord left_coord = {static_cast<int>(mm_radar_coord.x * px_per_mm_ +
-                                             (cos(DegToRad(left_deg)) * dist)),
-                            static_cast<int>(mm_radar_coord.y * px_per_mm_ +
-                                             (sin(DegToRad(left_deg)) * dist))};
+  PTIT::Coord left_coord = {
+      static_cast<int>(mm_radar_coord.x * px_per_mm_ +
+                       (cos(PTIT::DegToRad(left_deg)) * dist)),
+      static_cast<int>(mm_radar_coord.y * px_per_mm_ +
+                       (sin(PTIT::DegToRad(left_deg)) * dist))};
 
   PTIT::Coord right_coord = {
       static_cast<int>(mm_radar_coord.x * px_per_mm_ +
-                       (cos(DegToRad(right_deg)) * dist)),
+                       (cos(PTIT::DegToRad(right_deg)) * dist)),
       static_cast<int>(mm_radar_coord.y * px_per_mm_ +
-                       (sin(DegToRad(right_deg)) * dist))};
-
-  size_t call_num = sectors_color_.size();
-  sectors_color_.push_back(GetRandomColor());
+                       (sin(PTIT::DegToRad(right_deg)) * dist))};
 
   auto border_graphic = PTIT::Segment(left_coord, right_coord).GetGraphic();
   for (auto iter = border_graphic.begin(); iter != --border_graphic.end();
@@ -104,14 +64,15 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
 
     bool valid = true;
 
-    for (const auto& [x, y] : GetTriangle(radar_coord, border[0], border[1])) {
+    for (const auto& [x, y] :
+         GetSortedTriangle(radar_coord, border[0], border[1])) {
       if (!IsPixelInMap(x, y)) {
         continue;
       }
 
-      PixelData& pixel = map_[x][y];
+      BM::Bitmap::ScanData pixel = bitmap_.GetScanPoint(x, y);
 
-      if (pixel.call_identifier == call_num) {
+      if (pixel.call_identifier == measure_num_) {
         continue;
       }
 
@@ -124,9 +85,9 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
       if (pixel.dist >= px_dist) {
         pixel.dist = px_dist;
         pixel.is_border = false;
-        pixel.call_identifier = call_num;
-        SetPixel(x, y, sectors_color_.back());
+        pixel.call_identifier = measure_num_;
       }
+      bitmap_.SetScanData(x, y, pixel);
     }
 
     if (!valid) {
@@ -137,8 +98,9 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
         continue;
       }
       double px_dist = PTIT::GetDistance(radar_coord, border[i]);
-      PixelData& pixel = map_[border[i].x][border[i].y];
-      if (pixel.dist >= px_dist || pixel.call_identifier == call_num) {
+      BM::Bitmap::ScanData pixel =
+          bitmap_.GetScanPoint(border[i].x, border[i].y);
+      if (pixel.dist >= px_dist || pixel.call_identifier == measure_num_) {
         for (int xb = border[i].x - (border_thickness_ / 2);
              xb <=
              border[i].x + (border_thickness_ / 2) + (border_thickness_ % 2);
@@ -150,74 +112,42 @@ void EnvShot::AddMeasure(double deg, int mm_dist, double deg_width,
             if (!IsPixelInMap(xb, yb)) {
               continue;
             }
-            pixel = map_[xb][yb];
+            pixel = bitmap_.GetScanPoint(xb, yb);
             pixel.dist = px_dist;
             pixel.is_border = true;
-            pixel.call_identifier = call_num;
-            SetPixel(xb, yb, {0, 0, 0});
+            pixel.call_identifier = measure_num_;
+            bitmap_.SetScanData(xb, yb, pixel);
           }
         }
       }
     }
   }
+
+  measure_num_ += 1;
 }
 
 void EnvShot::CreateImage(const char* image_name) const {
-  ::PTIT::CreateImage(image_name, map_, map_.size(), map_[0].size(),
-                      [this](const PixelData& data) {
-                        if (data.call_identifier == UINT64_MAX) {
-                          return PTIT::RGB{255, 255, 255};
-                        }
-                        if (data.is_border) {
-                          return PTIT::RGB{0, 0, 0};
-                        }
-                        return sectors_color_[data.call_identifier];
-                      });
-}
-
-PTIT::RGB EnvShot::GetRandomColor() noexcept {
-  static bool uninitialized = true;
-  if (uninitialized) {
-    srand(time(NULL));
-    uninitialized = false;
-  }
-
-  PTIT::RGB color;
-  color.red = rand() % (PTIT::RGB::kMaxColor / 2) + (PTIT::RGB::kMaxColor / 2);
-  color.green =
-      rand() % (PTIT::RGB::kMaxColor / 2) + (PTIT::RGB::kMaxColor / 2);
-  color.blue = rand() % (PTIT::RGB::kMaxColor / 2) + (PTIT::RGB::kMaxColor / 2);
-
-  short color_sum = color.red + color.green + color.blue;
-  if (color_sum > PTIT::RGB::kMaxColor * 3 - 90) {
-    short diff = color_sum - (PTIT::RGB::kMaxColor + 90);
-    color.red -= diff, color.green -= diff, color.blue -= diff;
-  }
-
-  return color;
-}
-
-void EnvShot::SetPixel(int x, int y, PTIT::RGB color) const noexcept {
-  if (bitmap_scan_color_ == nullptr || bitmap_scan_alpha_ == nullptr ||
-      bitmap_border_alpha_ == nullptr) {
-    return;
-  }
-
-  long alpha_pos = map_.size() * y + x;
-  long scan_pos = alpha_pos * 3;
-
-  if (color == PTIT::RGB{0, 0, 0}) {
-    bitmap_border_alpha_[alpha_pos] = 255;
-  } else {
-    bitmap_scan_color_[scan_pos] = color.red;
-    bitmap_scan_color_[scan_pos + 1] = color.green;
-    bitmap_scan_color_[scan_pos + 2] = color.blue;
-    bitmap_scan_alpha_[alpha_pos] = 255;
-
-    bitmap_border_alpha_[alpha_pos] = 0;
-  }
+  auto [x_size, y_size] = bitmap_.GetSize();
+  std::map<size_t, PTIT::RGB> colors;
+  PTIT::CreateImage(image_name, bitmap_, x_size, y_size,
+                    [this, &colors](const BM::Bitmap& bitmap, int x, int y) {
+                      auto pixel = bitmap.GetScanPoint(x, y);
+                      if (pixel.call_identifier == UINT64_MAX) {
+                        return PTIT::RGB{255, 255, 255};
+                      }
+                      if (pixel.is_border) {
+                        return PTIT::RGB{0, 0, 0};
+                      }
+                      if (colors.contains(pixel.call_identifier)) {
+                        return colors.operator[](pixel.call_identifier);
+                      }
+                      auto curr_color = BM::GetRandomColor();
+                      colors.insert({pixel.call_identifier, curr_color});
+                      return curr_color;
+                    });
 }
 
 bool EnvShot::IsPixelInMap(int x, int y) const noexcept {
-  return x >= 0 && y >= 0 && x < map_.size() && y < map_[0].size();
+  auto [x_size, y_size] = bitmap_.GetSize();
+  return x >= 0 && y >= 0 && x < x_size && y < y_size;
 }
--- map/env-shot.hpp
+++ map/env-shot.hpp
@@ -1,44 +1,26 @@
 #pragma once
 
+#include <image-creator.hpp>
+#include <primitives.hpp>
 #include <vector>
 
-#include "image-creator.hpp"
-#include "primitives.hpp"
+#include "bitmap.hpp"
 
 class EnvShot {
  public:
-  EnvShot(int size_x, int size_y, float px_per_mm, int border_thickness = 1,
-          unsigned char* bitmap_scan_color = nullptr,
-          unsigned char* bitmap_scan_alpha = nullptr,
-          unsigned char* bitmap_border_alpha = nullptr);
+  EnvShot(BM::Bitmap& bitmap, float px_per_mm, int border_thickness = 1);
 
   void AddMeasure(double deg, int mm_dist, double deg_width,
                   const PTIT::Coord& mm_radar_coord);
 
   void CreateImage(const char* image_name) const;
 
-  static PTIT::RGB GetRandomColor() noexcept;
-
  private:
-  struct PixelData {
-    bool is_border = false;
-    int dist = INT_MAX;
-    size_t call_identifier = UINT64_MAX;
-  };
-
-  std::vector<std::vector<PixelData>> map_;
-
-  std::vector<PTIT::RGB> sectors_color_;
   float px_per_mm_;
   int border_thickness_;
+  size_t measure_num_ = 0;
 
-  unsigned char* bitmap_scan_color_;
-  unsigned char* bitmap_scan_alpha_;
-  unsigned char* bitmap_border_alpha_;
-
-  void SetPixel(int x, int y, PTIT::RGB color) const noexcept;
+  BM::Bitmap& bitmap_;
 
   bool IsPixelInMap(int x, int y) const noexcept;
 };
-
-double DegToRad(double deg);
