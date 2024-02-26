1ea3567522bc7ca4db3c2e74eaadda9c7f7d0c2e
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-02-26 17:59:40 +0300

    Implemented main loop and ClientCommunication class

---
+++ car_computer/include/modules.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <chrono>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <queue>
+#include <tcp-server.hpp>
+#include <thread>
+
+#include "bitmap.hpp"
+#include "hardware_processor.hpp"
+#include "support.hpp"
+
+namespace ML {
+
+const std::chrono::milliseconds kSleepFail(100);
+const std::chrono::milliseconds kPositionDelay(20);
+
+const int kMmPerPx = 10;
+const float kRadarWidth = 1.8;  // deg
+const int kBorderOffset = 500 / kMmPerPx;
+
+class ClientCommunication {
+ public:
+  ClientCommunication(CM::Car& car);
+  ~ClientCommunication();
+
+  void Send(SPRT::MessageType type, const SPRT::Message& message);
+  std::optional<SPRT::Status> Receive();
+
+ private:
+  TCP::TcpServer server_ = TCP::TcpServer(SPRT::kClientPort);
+  TCP::TcpClient client_;
+
+  std::queue<std::pair<SPRT::MessageType, std::shared_ptr<SPRT::Message>>>
+      to_send_;
+  std::optional<SPRT::Status> received_status_;
+
+  std::thread tcp_actor_;
+  std::thread position_sender_;
+
+  std::mutex shared_data_;
+
+  bool is_active_ = true;
+
+  CM::Car& car_;
+
+  void TCPActor();
+  void PositionSender();
+};
+
+void MainLoop(ClientCommunication& client, BM::Bitmap& bitmap, CM::Car& car);
+
+}  // namespace ML
---
+++ car_computer/source/main_loop.cpp
@@ -0,0 +1,67 @@
+#include "env-shot.hpp"
+#include "map_analyzer.hpp"
+#include "modules.hpp"
+
+namespace ML {
+
+void MainLoop(ClientCommunication& client, BM::Bitmap& bitmap, CM::Car& car) {
+  SPRT::Status status = SPRT::Act;
+
+  EnvShot env_shot(bitmap, 1.0 / kMmPerPx);
+  MA::WorkingAreaProcessor working_area(bitmap, kBorderOffset);
+
+  while (true) {
+    auto new_status = client.Receive();
+    if (new_status.has_value()) {
+      new_status = status;
+    }
+
+    {
+      struct SPRT::Condition condition(status, SPRT::Scanning);
+      client.Send(SPRT::Condition, condition);
+    }
+
+    if (status == SPRT::Pause) {
+      std::this_thread::sleep_for(kSleepFail);
+      continue;
+    }
+    if (status == SPRT::Stop) {
+      break;
+    }
+
+    auto scan = car.Scan();
+    {
+      for (auto [angle, dist] : scan) {
+        struct SPRT::RadarScan radar_scan(angle, dist);
+        client.Send(SPRT::Scan, radar_scan);
+      }
+      struct SPRT::Condition condition(status, SPRT::Computing);
+      client.Send(SPRT::Condition, condition);
+    }
+
+    for (auto [angle, dist] : scan) {
+      env_shot.AddMeasure(angle, dist, kRadarWidth, car.GetPosition().first);
+    }
+
+    auto destination =
+        working_area.ProcessArea(car.GetPosition().first, kBorderOffset);
+    if (!destination.has_value()) {
+      status = SPRT::Stop;
+      struct SPRT::Condition condition(status, SPRT::Finish);
+      return;
+    }
+
+    auto route = MA::GetRoute(bitmap, destination.value());
+    {
+      struct SPRT::Route client_route(route);
+      client.Send(SPRT::Route, client_route);
+
+      struct SPRT::Condition condition(status, SPRT::MovingToPoint);
+      client.Send(SPRT::Condition, condition);
+    }
+
+    RouteMove(car, route, kMmPerPx);
+  }
+}
+
+}  // namespace ML
