cf43602c88383c6bcd42ed24e1dd8823993dbb1a
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-03-23 02:59:57 +0300

    SHARE: added constants

---
+++ share/constants.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace CONST {
+
+const int kPxSizeX = 1500;
+const int kPxSizeY = 1000;
+
+const float kMmPerPx = 5;
+
+const int kBorderOffset = 500;
+const int kDistThreshold = 500;
+
+const std::string kServerAddress = "192.168.1.100";
+const int kTcpCommPort = 44'450;
+
+}  // namespace CONST
