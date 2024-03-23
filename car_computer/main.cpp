a056c6facff44e765685ee9464e1d2de79073b36
glebboyko <115988384+glebboyko@users.noreply.github.com>
2024-03-23 23:52:07 +0300

    MAIN: changed architecture

--- car_computer/main.cpp
+++ car_computer/main.cpp
@@ -1,12 +1,10 @@
+#include "constants.hpp"
 #include "control_center.hpp"
 #include "runners.hpp"
 
-const int kSizeX = 1500;
-const int kSizeY = 1000;
-const float kMmPerPx = 0.1;
-
 int main() {
-  CC::ControlCenter control_center(kSizeX, kSizeY, kMmPerPx);
+  CC::ControlCenter control_center(CONST::kPxSizeX, CONST::kPxSizeX,
+                                   CONST::kMmPerPx);
 
   RNS::AutoMapBuilder(control_center);
 }
